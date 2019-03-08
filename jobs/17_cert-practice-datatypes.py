cdb41fdf44e56046b8882a19f85d4d99fe738b9a
Kanchan Tewary <kanchan.tewary@gmail.com>
2019-03-08 11:09:08 +0530

    certification practice work

--- jobs/17_cert-practice-datatypes.py
+++ jobs/17_cert-practice-datatypes.py
@@ -1,7 +1,7 @@
-#spark certification practice - work on retail data, structured streaming, Definitive Guide chapter-2
+#spark certification practice - Spark Definitive Guide chapter-6
 
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import desc,col,window,column,date_format
+from pyspark.sql.functions import desc,col,window,column,date_format,pow,round,bround,corr,coalesce
 import time
 
 spark = SparkSession.Builder().appName("test13").master("local[3]").getOrCreate()
@@ -57,27 +57,102 @@ df.withColumn("isExpensive", DOTCodeFilter & (priceFilter|descripFilter))\
 
 from pyspark.sql.functions import expr
 
-df.withColumn("isExpensive",expr(NOT UnitPrice <= 250))\
+df.withColumn("isExpensive",expr("NOT UnitPrice <= 250"))\
         .where("isExpensive")\
         .select("Description","UnitPrice")\
         .show(5)
 
 #handling null data in boolean expressions
 
+df.where(col("Description").eqNullSafe("hello")).show()
 
 #working with numbers
 
+fabricatedQuantity = pow(col("Quantity") * col("UnitPrice"),2) + 5
+df.select(expr("CustomerId"),fabricatedQuantity.alias("realQuantity")).show()
+
+##rounding
+
+df.select(round(col("UnitPrice"),1).alias("rounded")).show(5)
+df.select(round(lit("2.5")).alias("rounded"),bround(lit("2.5")).alias("brounded")).show(2)
+
+##basic statistics
+
+df.describe().show()
+
+##using the stat package
+
+###find correlation
+
+df.stat.corr("UnitPrice","Quantity")
+df.select(corr("Quantity","UnitPrice")).show()
+
+from pyspark.sql.functions import count,mean,max,min,stddev_pop
+
+quantileProbs=[0.5]
+relError=0.05
+df.stat.approxQuantile("UnitPrice",quantileProbs,relError)
+df.stat.crosstab("StockCode","Quantity").show()
+df.stat.freqItems(["StockCode","Quantity"]).show()
+
+##creating a unique sequence id
+
+from pyspark.sql.functions import monotonically_increasing_id
+
+df.select(monotonically_increasing_id()).show(10)
 
 #working with strings
 
+#perform case converstions
+
+from pyspark.sql.functions import initcap,lower,upper,ltrim,rtrim,trim,lpad,rpad
+df.select(initcap(col("Description"))).show(5)
+df.select(col("Description"),initcap(col("Description")),lower(col("Description")),upper(col("Description"))).show(5)
+
+string_with_space="     hello     "
+
+df.select(ltrim(lit(string_with_space)),rtrim(lit(string_with_space)),trim(lit(string_with_space))).show()
 
 #regular expressions
 
 
 #working with dates, timestamps
+from pyspark.sql.functions import current_date,current_timestamp,date_add,date_sub,datediff,months_between,to_date,to_timestamp
 
+dateDF=spark.range(10).withColumn("today",current_date()).withColumn("now",current_timestamp())
+dateDF.show()
+
+dateDF.select(date_add(col("today"),5).alias("today+5"),date_sub(col("today"),5).alias("today-5")).show()
+
+#convert string to date, default format is 'YYYY-MM-DD'
+spark.range(1).select(to_date(lit("2019-02-01")).alias("start_date"),to_date(lit("2019-03-06")).alias("end_date")).select(datediff(col("start_date"),col("end_date"))).show()
+
+cleanDateDF=spark.range(1).withColumn("date1",current_date())
+date_format='YYYY-MM-DD'
+cleanDateDF.select(to_timestamp(col("date1"),date_format)).show()
 
 #working with nulls in data
+
+#coalesce, returns first not null value from a set of columns
+
+df.select(coalesce(col("Description"),col("customerId"))).show()
+
+#ifnull,nullif,nvl,nvl2
+
+#drop - removes rows that contain null
+
+#use the na subpackage
+
+df.na.drop("all") #drop if all of the columns have null
+df.na.drop("any") #drop if any of the columns have null
+df.na.drop("all",subset=["InvoiceNo","StockCode"]) #drop if there is null in a subset of columns
+
+df.na.fill("Not Available") #replace all null values to 'Not Available'
+
+fill_col_vals={"StockCode":5,"InvoiceNo":"No value"}
+df.na.fill(fill_col_vals) #fill null selectively
+
+df.na.replace([" "],"UNKNOWN","Description") #replace space to UNKNOWN
 
 
 #working with complex types
--- jobs/18_cert-practice-aggegations.py
+++ jobs/18_cert-practice-aggegations.py
@@ -1,8 +1,21 @@
 #spark definitive guide chapter 7 - aggregations
 
+#create the spark session object
+from pyspark.sql import SparkSession
+spark = SparkSession.Builder().appName("chapter7").master("local[3]").getOrCreate()
+
+#create the retail data fram to work with
+
+df=spark.read.format("csv")\
+        .option("header","true")\
+        .option("inferSchema","true")\
+        .load("/home/user/workarea/projects/Spark-The-Definitive-Guide/data/retail-data/all/*.csv")\
+        .coalesce(5)
+df.cache()
 
 #taking count
 
+print(df.count())
 
 #count distinct
 
