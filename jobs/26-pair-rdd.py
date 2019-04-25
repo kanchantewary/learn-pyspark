afd277356de3c4389132c5a583c5fef634716949
Kanchan Tewary <kanchan.tewary@gmail.com>
2019-04-25 09:45:58 +0530

    rdd practice

--- jobs/26-pair-rdd.py
+++ jobs/26-pair-rdd.py
@@ -7,7 +7,7 @@ spark = SparkSession.Builder().appName("pair-rdd").master("local[2]").getOrCreat
 sc = spark.sparkContext
 
 #create
-r1 = sc.parallelize([("a",1),("b",2),("c",3),("d",4)])
+r1 = sc.parallelize([("a",1),("b",2),("c",3),("d",4),("a",5),("c",6),("c",2)])
 print(r1.collect())
 print(type(r1))
 
@@ -18,7 +18,23 @@ print(r1.values().collect())
 #transform
 
 #reducebykey
+r2 = r1.reduceByKey(lambda x,y:x+y)
+
+print(r2.collect())
+
+print(r2.toDebugString().decode("utf-8"))
+
 #groupbykey
+
+r3 = r1.groupByKey()
+
+print(r3.collect())
+
+r4 = r3.map(lambda x:(x[0],list(x[1])))
+
+print(r4.toDebugString().decode("utf-8"))
+print(r4.collect())
+
 #combinebykey
 #mapvalues
 #flatmapvalues
