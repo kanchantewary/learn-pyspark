a399cfe886f3c7a1955bfa49409ec248921e73e4
Kanchan Tewary <kanchan.tewary@gmail.com>
2019-04-25 18:57:15 +0530

    pair rdd

--- jobs/26-pair-rdd.py
+++ jobs/26-pair-rdd.py
@@ -35,14 +35,67 @@ r4 = r3.map(lambda x:(x[0],list(x[1])))
 print(r4.toDebugString().decode("utf-8"))
 print(r4.collect())
 
-#combinebykey
 #mapvalues
+
+r5 = r1.mapValues(lambda x:x**2)
+
+print(r5.collect())
+
 #flatmapvalues
+
 #sortbykey
+
+
+#combinebykey
+
+createCombiner = 'lambda x:(x,1)'
+mergeValue = 'lambda x,y:(x[0]+y,x[1]+1)'
+mergeCombiner = 'lambda x,y:(x[0]+y[0],x[1],y[1])'
+
+#r6 = r5.combineByKey(createCombiner,mergeValue,mergeCombiner)
+r6 = r5.combineByKey((lambda x:(x,1)),(lambda x,y:(x[0]+y,x[1]+1)),(lambda x,y:(x[0]+y[0],x[1]+y[1])))
+
+print(r6.collect())
+#print(r6.map(lambda x,y:(x,y[0]/y[1])).collectAsMap())
 
 #subtractbykey
 #join
 #rightouterjoin
 #leftouterjoin
 #cogroup
+
+#foldbykey
+
+r7 = sc.parallelize([('a',1),('b',1),('a',1)],3)
+from operator import add
+print(r7.foldByKey(1,add).collect())
+print(r7.foldByKey(1,(lambda x,y:x+y)).collect())
+print(r7.foldByKey(2,(lambda x,y:x*y)).collect())
+
+print(f'id of r7 rdd is {r7.id()}')
+print(f'r7 is empty - true/false - {r7.isEmpty()}')
+
+#groupby
+
+r8 = sc.parallelize(['apple','guava','cat','america','care','great'],3)
+
+print(r8.groupBy(lambda x:x[0]).collect())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
