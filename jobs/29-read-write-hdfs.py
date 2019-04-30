6c5f480b886e93bb4836c6afdc20e0f1d60ddc0d
Kanchan Tewary <kanchan.tewary@gmail.com>
2019-04-30 14:09:07 +0530

    modified permissions

--- jobs/29-read-write-hdfs.py
+++ jobs/29-read-write-hdfs.py
@@ -10,7 +10,12 @@ spark = SparkSession.Builder().appName("hdfs").master("local[2]").getOrCreate()
 
 sc = spark.sparkContext
 
-r3 = sc.parallelize(["spark","hadoop","mongo","hive","nifi"])
-r3.saveAsTextFile("hdfs://localhost:9000/test/sparkrdd.txt")
+r1 = sc.parallelize(["spark","hadoop","mongo","hive","nifi"])
+r1.saveAsTextFile("hdfs://localhost:9000/test/sparkrdd.txt")
 
+# create an rdd from hdfs file, write after applying filter
+
+r2 = sc.textFile("hdfs://localhost:9000/test/*.log").filter(lambda x:"ERR" in x)
+
+r2.saveAsTextFile("hdfs://localhost:9000/test/errors.txt")
 
